df9d8ada618a3f1dccbd6c83be03a2aee47ef4b0
Aleksandar <aleksandar.exunys@gmail.com>
2023-08-31 04:07:55 +0200

    Launcher

--- src/ESP.lua
+++ src/ESP.lua
@@ -1,24 +1,16 @@
 local game = game
 local select, pcall, loadstring , warn = select, pcall, loadstring, warn
 
-if ExunysDeveloperESP then
-	warn("EXUNYS_ESP > Loader - Exunys ESP Module is already loaded into your environment!")
-	warn("EXUNYS_ESP > Loader - Execute \"ExunysDeveloperESP:Exit()\" before running a new instance of this module.")
-else
-	if select(2, pcall(select(2, pcall(loadstring, game:HttpGet("https://raw.githubusercontent.com/Exunys/Exunys-ESP/main/src/Modules/Original.lua"))))) then
-		if ExunysDeveloperESP then
-			ExunysDeveloperESP:Exit()
-		end
+local Success, ESP = pcall(select(2, pcall(loadstring, game:HttpGet("https://raw.githubusercontent.com/Exunys/Exunys-ESP/main/src/Modules/Original.lua"))))
 
-		if select(2, pcall(select(2, pcall(loadstring, game:HttpGet("https://raw.githubusercontent.com/Exunys/Exunys-ESP/main/src/Modules/UWP%20Support.lua"))))) then
-			warn("EXUNYS_ESP > Loader - Your script execution software does not support this module.")
-			ExunysDeveloperESP:Exit()
-		else
-			return loadstring(game:HttpGet("https://raw.githubusercontent.com/Exunys/Exunys-ESP/main/src/Modules/UWP%20Support.lua"))()
-		end
-	else
-		return loadstring(game:HttpGet("https://raw.githubusercontent.com/Exunys/Exunys-ESP/main/src/Modules/Original.lua"))()
-	end
+if not Success then
+    Success, ESP = pcall(select(2, pcall(loadstring, game:HttpGet("https://raw.githubusercontent.com/Exunys/Exunys-ESP/main/src/Modules/UWP%20Support.lua"))))
+
+    if not Success then
+        return warn("EXUNYS_ESP > Loader - Your script execution software does not support this module.")
+    end
 end
+
+return ESP
 
 -- Exunys <3
