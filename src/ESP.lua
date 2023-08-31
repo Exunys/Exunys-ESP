68254780e1a22fa9ce150969f413cf436ecaa8fe
Aleksandar <aleksandar.exunys@gmail.com>
2023-08-31 03:14:51 +0200

    Launcher

--- src/ESP.lua
+++ src/ESP.lua
@@ -2,9 +2,11 @@ local game = game
 local select, pcall, loadstring , warn = select, pcall, loadstring, warn
 
 if select(2, pcall(select(2, pcall(loadstring, game:HttpGet("https://raw.githubusercontent.com/Exunys/Exunys-ESP/main/src/Modules/Original.lua"))))) then
+	ExunysDeveloperESP:Exit()
 	if select(2, pcall(select(2, pcall(loadstring, game:HttpGet("https://raw.githubusercontent.com/Exunys/Exunys-ESP/main/src/Modules/UWP%20Support.lua"))))) then
 		if not ExunysDeveloperESP then
 			warn("EXUNYS_ESP > Loader - Your script execution software does not support this module.")
+			ExunysDeveloperESP:Exit()
 		else
 			warn("EXUNYS_ESP > Loader - Exunys ESP Module is already loaded into your environment!")
 			warn("EXUNYS_ESP > Loader - Execute \"ExunysDeveloperESP:Exit()\" before running a new instance of this module.")
