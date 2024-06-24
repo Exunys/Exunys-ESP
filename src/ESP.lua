7eb2c25857a72cff5df529d4d45b1dc79f87bb78
Aleksandar <aleksandar.exunys@gmail.com>
2024-06-24 04:04:36 +0200

    Added support for free / weaker exploits

--- src/ESP.lua
+++ src/ESP.lua
@@ -1,12 +1,18 @@
 local game = game
 local select, pcall, loadstring , warn = select, pcall, loadstring, warn
 
-local Success, ESP = pcall(select(2, pcall(loadstring, game:HttpGet("https://raw.githubusercontent.com/Exunys/Exunys-ESP/main/src/Modules/Original.lua"))))
+local Success, ESP = pcall(select(2, pcall(loadstring, game:HttpGet("https://raw.githubusercontent.com/Exunys/Exunys-ESP/main/src/Modules/Original.lua")))) -- Original / best version
 
 if not Success then
-    Success, ESP = pcall(select(2, pcall(loadstring, game:HttpGet("https://raw.githubusercontent.com/Exunys/Exunys-ESP/main/src/Modules/UWP%20Support.lua"))))
+    warn("EXUNYS_ESP > Loader - Your script execution software does not support the original / best version, trying the degraded version...")
+    
+    Success, ESP = pcall(select(2, pcall(loadstring, game:HttpGet("https://raw.githubusercontent.com/Exunys/Exunys-ESP/main/src/Modules/UWP%20Support.lua")))) -- Degraded for UWP (works on web version)
 
     if not Success then
+        warn("EXUNYS_ESP > Loader - Your script execution software does not support the degraded version. Trying the primitive version.")
+
+        Success, ESP = pcall(select(2, pcall(loadstring, game:HttpGet("https://raw.githubusercontent.com/Exunys/Exunys-ESP/main/src/Modules/Primitive.lua")))) -- Degraded for UWP & free / weaker exploits (works on web version)
+
         return warn("EXUNYS_ESP > Loader - Your script execution software does not support this module.")
     end
 end
