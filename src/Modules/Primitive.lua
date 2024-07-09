3ef26968674f132e238504abe5a5a428c65148e6
Aleksandar <aleksandar.exunys@gmail.com>
2024-07-09 12:11:05 +0200

    Better landing font for worse exploits

--- src/Modules/Primitive.lua
+++ src/Modules/Primitive.lua
@@ -155,7 +155,7 @@ getgenv().ExunysDeveloperESP = {
 			Color = Color3fromRGB(255, 255, 255),
 			Transparency = 1,
 			Size = 14,
-			Font = DrawingFonts.System, -- UI, System, Plex, Monospace
+			Font = DrawingFonts.Plex, -- UI, System, Plex, Monospace
 
 			OutlineColor = Color3fromRGB(0, 0, 0),
 			Outline = true,
